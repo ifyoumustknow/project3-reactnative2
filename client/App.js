b74eacba5f0cbb2b1b684425683318419c1088b2
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2020-02-29 13:38:42 -0800

    We have a purple background!

--- client/App.js
+++ client/App.js
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
+import HomeScreen from './screens/HomeScreen';
 import useLinking from './navigation/useLinking';
 
 const Stack = createStackNavigator();
@@ -48,12 +48,7 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <HomeScreen/>
       </View>
     );
   }
