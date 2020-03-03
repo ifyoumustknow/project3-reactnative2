a67a2bf72ff85d8c5af41c5eeb7ad04d4c62ce61
Rob Tang <ifyoumustknow@gmail.com>
2020-03-02 18:19:58 -0800

    updated gameboard

---
+++ client/components/Bird.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Image } from 'react-native';
+
+export default class Bird extends Component {
+  render() {
+    const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
+    const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
+    const x = this.props.body.position.x - width / 2;
+    const y = this.props.body.position.y - height / 2;
+
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: y,
+          left: x,
+          width: width,
+          height: height,
+          backgroundColor: this.props.color
+        }}
+      />
+      
+    );
+  }
+}
